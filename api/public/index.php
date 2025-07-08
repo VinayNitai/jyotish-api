c5c6abb31c1cc3c0c193d99d76f0f6ad7cbf957d
VinayNitai <tecnico@108porcento.com>
2025-07-08 19:07:59 -0300

    Update index.php

--- api/public/index.php
+++ api/public/index.php
@@ -1,15 +1,8 @@
 <?php
 
 use App\Kernel;
-use Symfony\Component\HttpFoundation\Request; // <-- LINHA ADICIONADA
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
-
-// Bloco adicionado para confiar nos proxies do Coolify/Nginx
-Request::setTrustedProxies(
-    ['127.0.0.1', 'REMOTE_ADDR'],
-    Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_PROTO | Request::HEADER_X_FORWARDED_HOST | Request::HEADER_X_FORWARDED_PORT | Request::HEADER_X_FORWARDED_PREFIX
-);
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
